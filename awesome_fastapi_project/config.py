a32aca2647dfa34b6cf6653e836d08e457d40867
Bohdan Agarkov <sidi0us.mail@gmail.com>
2023-08-02 18:57:54 +0300

    Added some database string debug logic

--- awesome_fastapi_project/config.py
+++ awesome_fastapi_project/config.py
@@ -1,11 +1,19 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    database_url: str
+    production_database_url: str
+    test_database_url: str
+    database_url: str = "sqlite://:memory:"
     secret_key: str
-    debug: bool
+    debug: bool = True
 
     class Config:
         env_file = ".env"
 
 config = Settings()
+
+if config.debug:
+    config.database_url = config.test_database_url
+
+else:
+    config.database_url = config.production_database_url
--- awesome_fastapi_project/main.py
+++ awesome_fastapi_project/main.py
@@ -9,4 +9,6 @@ from .logging import logger
 app.include_router(user_router, prefix="/user", tags=["user"])
 app.include_router(posts_router, prefix="/posts", tags=["posts"])
 
-logger.debug(f"Databse URL: {config.database_url}")
+logger.debug(f"Production Databse URL: {config.production_database_url}")
+logger.debug(f"Test Database URL: {config.test_database_url}")
+logger.debug(f"Database URL: {config.database_url}")
