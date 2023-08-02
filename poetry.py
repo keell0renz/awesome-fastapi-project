c6c5b190b07cf7ca42e43b657d3196593d1a72dd
Bohdan Agarkov <sidi0us.mail@gmail.com>
2023-08-02 18:34:29 +0300

    Added poetry run test script

--- poetry.py
+++ poetry.py
@@ -1,6 +1,12 @@
+import sys
 import os
 
 def run_dev(): # Run development server
     os.system(
         "uvicorn awesome_fastapi_project.main:app"
+    )
+
+def run_pytest(): # Run pytest
+    os.system(
+        "pytest"
     )
