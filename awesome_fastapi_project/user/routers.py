344829a50fdd5a1ecbd7b22209d19ca4d6218509
Bohdan Agarkov <sidi0us.mail@gmail.com>
2023-08-02 18:45:11 +0300

    Added mock user endpoints + tests

--- awesome_fastapi_project/user/routers.py
+++ awesome_fastapi_project/user/routers.py
@@ -1,3 +1,26 @@
 from fastapi import APIRouter
+from fastapi_utils.cbv import cbv
 
 router = APIRouter()
+
+@cbv(router)
+class UserView:
+    @router.get("/")
+    def get_users(self):
+        return {"message": "Hello World! Are you lost? This is not the page you're looking for."}
+
+    @router.post("/")
+    def create_user(self):
+        return {"message": "Hello World! You just created a user. Congratulations, you're now a parent!"}
+
+    @router.get("/{user_id}")
+    def get_user(self, user_id: int):
+        return {"message": f"Hello World! You're looking for user {user_id}? Sorry, they're on vacation in Hawaii."}
+
+    @router.put("/{user_id}")
+    def update_user(self, user_id: int):
+        return {"message": f"Hello World! User {user_id} has been updated. They now have a new hat."}
+
+    @router.delete("/{user_id}")
+    def delete_user(self, user_id: int):
+        return {"message": f"Hello World! User {user_id} has been deleted. Don't worry, they'll be back in the sequel."}
