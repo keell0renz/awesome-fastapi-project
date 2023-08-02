b887b481e1961ec56e5fe412dec928bcaa355b7e
Bohdan Agarkov <sidi0us.mail@gmail.com>
2023-08-02 19:56:05 +0300

    Edited models

--- awesome_fastapi_project/user/models.py
+++ awesome_fastapi_project/user/models.py
@@ -5,8 +5,6 @@ class User(Model):
     id = fields.IntField(pk=True)
     username = fields.CharField(max_length=100, unique=True)
     password = fields.CharField(max_length=100)
-    is_active = fields.BooleanField(default=True)
-    is_superuser = fields.BooleanField(default=False)
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
 
