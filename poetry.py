c35919aaea83a6fe564a5f74a391d1eade04c078
Bohdan Agarkov <sidi0us.mail@gmail.com>
2023-08-02 19:27:43 +0300

    Added migration shortcuts

--- poetry.py
+++ poetry.py
@@ -10,3 +10,13 @@ def run_pytest(): # Run pytest
     os.system(
         "pytest"
     )
+
+def run_migrate(): # Run migrations
+    os.system(
+        "aerich migrate && aerich upgrade"
+    )
+
+def run_init_db(): # Initialize database
+    os.system(
+        "aerich init-db"
+    )
