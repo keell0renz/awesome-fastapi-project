d70be6b4d6186b3da3eea77ca9d2483e43e83518
Bohdan Agarkov <sidi0us.mail@gmail.com>
2023-08-04 16:49:31 +0300

    Fix

--- awesome_fastapi_project/user/authorization.py
+++ awesome_fastapi_project/user/authorization.py
@@ -2,7 +2,7 @@ from awesome_fastapi_project.exceptions import UserIsNotAuthenticated
 
 class Ensure:
     @staticmethod
-    def user_is_not_anonymous(self, user):
+    def user_is_not_anonymous(user):
         """
             Ensure user is not anonymous.
         """
