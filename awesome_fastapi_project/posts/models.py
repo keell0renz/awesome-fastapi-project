ab1d4ba2118dd60722eb1d96cf14fe5dee2c72d5
Bohdan Agarkov <sidi0us.mail@gmail.com>
2023-08-04 17:08:31 +0300

    Added models to post

---
+++ awesome_fastapi_project/posts/models.py
@@ -0,0 +1,12 @@
+from tortoise import fields
+from tortoise.models import Model
+
+class Post(Model):
+    id = fields.IntField(pk=True)
+    title = fields.CharField(max_length=255)
+    content = fields.TextField()
+    created_at = fields.DatetimeField(auto_now_add=True)
+    updated_at = fields.DatetimeField(auto_now=True)
+
+    class PydanticMeta:
+        exclude = ["created_at", "updated_at"]
